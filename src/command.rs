98ba19c9c29b923019d1ca3cf1035c867f626114
Dragoslav Tamindžija <dragodev@protonmail.com>
2025-02-18 18:43:10 +0100

    feat: basic command

---
+++ src/command.rs
@@ -0,0 +1,17 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(name = "traceroute-rs", version = "1.0", about = "Follow your packet across the internet")]
+pub struct Command {
+
+    #[arg(long)]
+    pub v4: bool,
+
+    #[arg(long)]
+    pub v6: bool,
+
+    #[arg(short, long, default_value_t = 5000)]
+    pub timeout: u64,
+
+    pub address: String,
+}
